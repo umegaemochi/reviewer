a119d2430d8a908c22a2d6e26d165be11115afec
umegaemochi <soda.0924.cream@gmail.com>
2026-08-17 21:17:15 +0900

    CrossOriginの追加

--- backend/src/main/java/com/example/backend/controller/AuthController.java
+++ backend/src/main/java/com/example/backend/controller/AuthController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     private final UserRepository userRepository;
